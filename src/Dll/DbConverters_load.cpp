8dd6f49071a76034c5f355f8e4767fec82d947ed
heather <>
2000-12-06 03:30:12 +0000

    new files to help handle writing Root files from TDS data

---
+++ src/Dll/DbConverters_load.cpp
@@ -0,0 +1,42 @@
+//====================================================================
+//  DbCnv_load.cpp
+//--------------------------------------------------------------------
+//
+//  Package    : DbCnv
+//
+//  Description: Implementation of <Package>_load routine. This routine 
+//               is needed for forcing the linker to load all the components
+//               of the library.. 
+//
+//====================================================================
+
+#include "Gaudi/Interfaces/ICnvFactory.h"
+#include "Gaudi/Interfaces/ISvcFactory.h"
+#include "Gaudi/Interfaces/IAlgFactory.h"
+
+#define DLL_DECL_OBJECT(x)     extern const IFactory&    x##Factory; x##Factory.addRef();
+#define DLL_DECL_SERVICE(x)    extern const ISvcFactory& x##Factory; x##Factory.addRef();
+#define DLL_DECL_CONVERTER(x)  extern const ICnvFactory& DbOOMs##x##Factory; DbOOMs##x##Factory.addRef();
+#define DLL_DECL_ALGORITHM(x)  extern const IAlgFactory& x##Factory; x##Factory.addRef();
+
+void DbConverters_load()     {
+
+  // Declare all the services
+  DLL_DECL_SERVICE( DbEventCnvSvc );
+  DLL_DECL_SERVICE( DbEvtSelector );
+  
+  // Now declare all availible converters
+  DLL_DECL_CONVERTER( EventCnv );
+  DLL_DECL_CONVERTER( MCEventCnv );
+  DLL_DECL_CONVERTER( MCACDHitVectorCnv );
+  DLL_DECL_CONVERTER( MCTKRHitVectorCnv );
+  DLL_DECL_CONVERTER( MCCalorimeterHitVectorCnv );
+  //DLL_DECL_CONVERTER( AxPartCandidateVectorCnv );
+  //DLL_DECL_CONVERTER( ExportConverter );
+  //DLL_DECL_CONVERTER( ExportVectorCnv );
+  //DLL_DECL_CONVERTER( ExportListCnv );
+} 
+
+extern "C" void DbConverters_loadRef() {
+  DbConverters_load();
+}
