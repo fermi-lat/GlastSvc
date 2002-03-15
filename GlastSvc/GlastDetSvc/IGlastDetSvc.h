5597f3f943b8d48b53585b6e798046c286e209fe
burnett <>
2002-03-15 15:28:44 +0000

    clean out dead code

--- GlastSvc/GlastDetSvc/IGlastDetSvc.h
+++ GlastSvc/GlastDetSvc/IGlastDetSvc.h
@@ -1,4 +1,4 @@
-// $Id: IGlastDetSvc.h,v 1.9 2002/03/13 09:05:48 riccardo Exp $
+// $Id: IGlastDetSvc.h,v 1.10 2002/03/14 12:31:43 riccardo Exp $
 // 
 //!  \author: Sawyer Gillespie  hgillesp@u.washington.edu
 //
@@ -35,31 +35,10 @@ public:
     //! new detModel interface, will call back. 
     virtual void accept(IMedia& media)=0;
 
-
     /// retrive the 3D transformation of a volume given a valid ID
     virtual StatusCode getTransform3DByID(idents::VolumeIdentifier,HepTransform3D*)=0;
 
-#if 0
-    //! open an IRF file
-    virtual StatusCode openIRF(std::string filename)=0;
-    
-    //! read the next event from the IRF file return fail if EOF
-    virtual StatusCode  readIRF ()=0;
-
-    
-    //! return the number of detectors currently with data
-    virtual int detectors_with_data()const=0;
-  
-    //! accept a visitor to traverse the structure note that must be on a const object
-    virtual void accept(DetectorConverter&)const=0;
-
-    //! access to the IFile containing detector constants
-    virtual const xml::IFile* iniFile()const=0;
-
-    //! set new root detector
-    virtual void setDetector(GlastDetector* d)=0;
-#endif
-	    /// Retrieve interface ID
+    /// Retrieve interface ID
     static const InterfaceID& interfaceID() { return IID_IGlastDetSvc; }
 
 };
