a211a022a453a12af3ab00d3f4b0d557c3dcdfa0
heather <>
2002-03-15 21:13:31 +0000

    use Gaudi v9 macros for loading shareables and removing old definition of IGlastDetSvc ID

--- src/Dll/GlastSvc_dll.cpp
+++ src/Dll/GlastSvc_dll.cpp
@@ -1,36 +1,8 @@
-//====================================================================
-//  GlastSvc_dll.cpp
-//--------------------------------------------------------------------
-//
-//  Package    : GlastSvc
-//
-//  Description: Implementation of DllMain routine.
-//               The DLL initialisation must be done seperately for 
-//               each DLL. 
-//
-//
-//====================================================================
+/** @file GlastSvc_dll.cpp
 
-// DllMain entry point
-#include "GaudiKernel/DllMain.icpp"
-#include <iostream>
-void GaudiDll::initialize(void*) 
-{
-}
+  $Header$
+*/
 
-void GaudiDll::finalize(void*) 
-{
-}
-extern void GlastSvc_load();
-#include "GaudiKernel/FactoryTable.h"
-
-extern "C" FactoryTable::EntryList* getFactoryEntries() {
-  static bool first = true;
-  if ( first ) {  // Don't call for UNIX
-    GlastSvc_load();
-    first = false;
-  }
-  return FactoryTable::instance()->getEntries();
-} 
-
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(GlastSvc)
 
--- src/Dll/GlastSvc_load.cpp
+++ src/Dll/GlastSvc_load.cpp
@@ -1,48 +1,20 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/GlastSvc/src/Dll/GlastSvc_load.cpp,v 1.16 2001/08/23 06:03:06 burnett Exp $
-//====================================================================
-//  GlastSvc_load.cpp
-//--------------------------------------------------------------------
-//
-//  Package    : Gaudi/System
-//
-//  Description: Implementation of <Package>_load routine.
-//               This routine is needed for forcing the linker
-//               to load all the components of the library. 
-//
-//====================================================================
+/** 
+* @file GlastSvc_load.cpp
+* @brief This is needed for forcing the linker to load all components
+* of the library.
+*
+*  $Header$
+*/
 
-#include "GaudiKernel/ICnvFactory.h"
-#include "GaudiKernel/ISvcFactory.h"
-#include "GaudiKernel/IAlgFactory.h"
+#include "GaudiKernel/DeclareFactoryEntries.h"
 
-
-#define DLL_DECL_SERVICE(x)    extern const ISvcFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_CONVERTER(x)  extern const ICnvFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_ALGORITHM(x)  extern const IAlgFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_OBJECT(x)     extern const IFactory& x##Factory; x##Factory.addRef();
-
-//! Load all GlastSvc services: 
-void GlastSvc_load() {
-    // GlastSvc services
-    DLL_DECL_SERVICE( GlastDetSvc );
-    DLL_DECL_SERVICE( GlastEventSelector );
-    DLL_DECL_SERVICE( EventCnvSvc );
-    //DLL_DECL_CONVERTER( MCACDHitCnv );
-    DLL_DECL_CONVERTER( EventCnv );
-    DLL_DECL_CONVERTER( MCEventCnv );
-
-    DLL_DECL_CONVERTER( McVertexCnv );
-   
-    DLL_DECL_CONVERTER( DigiEventCnv );
-
-    // DLL_DECL_CONVERTER( MCSiLayerCnv );
-    //DLL_DECL_CONVERTER( MCCalorimeterHitCnv );
-
- //   DLL_DECL_CONVERTER( LdGlastDataCnv );
-
+DECLARE_FACTORY_ENTRIES(GlastSvc) {
+    DECLARE_SERVICE( GlastDetSvc);
+    DECLARE_SERVICE( GlastEventSelector );
+    DECLARE_SERVICE( EventCnvSvc );
+    
+    DECLARE_CONVERTER( EventCnv );
+    DECLARE_CONVERTER( MCEventCnv );
+    DECLARE_CONVERTER( McVertexCnv );   
+    DECLARE_CONVERTER( DigiEventCnv );
 } 
-
-extern "C" void GlastSvc_loadRef()    {
-  GlastSvc_load();
-}
-
