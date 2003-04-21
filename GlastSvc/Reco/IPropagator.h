b7eae70cb9099cf01e43f80d490fb22bb9c160ef
usher <>
2003-04-21 22:43:49 +0000

    Add new propagator interface, for use with G4PropagationTool. Old interfaces preserved, this is just an alternate.

---
+++ GlastSvc/Reco/IPropagator.h
@@ -0,0 +1,94 @@
+#ifndef IPropagator_H
+#define IPropagator_H
+
+#include "GaudiKernel/IAlgTool.h"
+#include "Event/Recon/TkrRecon/TkrFitPar.h"
+#include "Event/Recon/TkrRecon/TkrFitMatrix.h"
+#include "idents/VolumeIdentifier.h"
+#include "geometry/Point.h"
+#include "geometry/Vector.h"
+#include <iostream>
+
+static const InterfaceID IID_IPropagator("IPropagator", 1 , 0);
+
+/** @class IPropagator
+* @brief Definition of the interface to the particle propagator
+*
+*
+* @author Tracy Usher
+* $Header: /nfs/slac/g/glast/ground/cvs/GlastSvc/GlastSvc/Reco/IPropagator.h,v 1.0 2003/01/23 21:31:58 usher Exp $
+*/
+class  IPropagator
+{
+public:
+  	/// Retrieve interface ID
+	static const InterfaceID& interfaceID() { return IID_IPropagator; }
+
+    //! Methods to intialize propagator before stepping 
+    //! Tracking from intitial point and direction
+    virtual void setStepStart(const Point& startPos, const Vector& startDir) = 0;
+    //! Tracking from initial parameters
+    virtual void setStepStart(const Event::TkrFitPar& trackPar, const Event::TkrFitMatrix& trackCov, double z) = 0;
+
+    //! Takes a step of distance given by arcLen
+    virtual void step(double arcLen) = 0;
+
+    //! Number of steps taken
+    virtual int getNumberSteps() const = 0;
+
+    //! Methods to return information after the step
+    //! Return position after stepping, arcLen can be less than step taken
+    virtual Point getPosition(double arcLen = -1.) const = 0;
+    //! returns the arc length for this step
+    virtual double getArcLen() const = 0;
+
+    //! return position at the center of the step in the volume
+    virtual Point getStepPosition(int stepIdx = -1) const = 0;
+    //! returns the arc length for this step
+    virtual double getStepArcLen(int stepIdx = -1) const = 0;
+
+    //! Return track parameters after stepping, arcLen can be less than step taken
+    virtual Event::TkrFitPar getTrackPar(double arcLen = -1.) const = 0;
+    //! Return track covariance matrix after stepping, arcLen can be less than step taken
+    //! *** NOTE *** this only has meaning if initial matrix given at setStepStart
+    virtual Event::TkrFitMatrix getTrackCov(int propDir, double momentum, double arcLen = -1.) const = 0;
+
+    //! Return multiple scattering matrix after stepping, arcLen can be less than step taken
+    virtual Event::TkrFitMatrix getMscatCov(double momentum, double arcLen = -1.) const = 0;
+
+    //! Return volume identifer after stepping
+    virtual idents::VolumeIdentifier getVolumeId(double arcLen = -1.) const = 0;
+    //! Return volume identifer at given step index
+    virtual idents::VolumeIdentifier getStepVolumeId(int stepIdx = -1) const = 0;
+
+    //! Return radiation lengths traversed
+    virtual double getRadLength(double arcLen = -1.) const = 0;
+    //! Return radiation lengths for this step
+    virtual double getStepRadLength(int stepIdx = -1.) const = 0;
+
+    //! Return the number of sensitive planes crossed to end of step
+    virtual int getNumSensePlanesCrossed() const = 0;
+
+    //! Is the point at the end of stepping inside an Active Area? 
+    virtual double isInsideActArea()   const = 0;
+    virtual double isInsideActLocalX() const = 0;
+    virtual double isInsideActLocalY() const = 0;
+
+    //! Is the plane at the end of stepping an X or a Y plane?
+    virtual bool   isXPlane()          const = 0;
+    virtual bool   isYPlane()          const = 0;
+
+    //! Is the strip at the end of stepping live?
+    virtual bool   isStripLive()       const = 0; 
+
+    /// dump current status, to the stream
+    virtual void printOn(std::ostream& str=std::cout )const=0;
+
+    virtual ~IPropagator(){};
+protected:
+    IPropagator(){};
+
+};
+
+#endif
+
