66a644e8d0c69d3f5d1b3de7a09c775a159eff83
usher <>
2004-12-16 05:28:09 +0000

    Updates to latest round of Tracker TDS class changes

--- GlastSvc/MonteCarlo/IMcGetTrackInfoTool.h
+++ GlastSvc/MonteCarlo/IMcGetTrackInfoTool.h
@@ -14,7 +14,7 @@
 #include "GaudiKernel/IAlgTool.h"
 #include "Event/MonteCarlo/McEventStructure.h"
 #include "Event/MonteCarlo/McRelTableDefs.h"
-#include "Event/Recon/TkrRecon/TkrPatCand.h"
+#include "Event/Recon/TkrRecon/TkrTrack.h"
 
 static const InterfaceID IID_IMcGetTrackInfoTool("IMcGetTrackInfoTool", 2 , 0);
 
@@ -25,19 +25,19 @@ class IMcGetTrackInfoTool : virtual public IAlgTool
     static const InterfaceID& interfaceID() { return IID_IMcGetTrackInfoTool; }
 
     /// @brief Return the number of McParticles associated to a given TkrPatCand track
-    virtual int                           getNMcParticles(const Event::TkrPatCand*)=0;
+    virtual int                           getNMcParticles(const Event::TkrTrack*)=0;
 
     /// @brief Return the vector of McParticles associated to a given TkrPatCand track
-    virtual const Event::McParticleRefVec getMcParticleVec(const Event::TkrPatCand*)=0;
+    virtual const Event::McParticleRefVec getMcParticleVec(const Event::TkrTrack*)=0;
 
     /// @brief Return TkrPatCand <-> McParticle "best" match
-    virtual const Event::McParticle*      getBestMcParticle(const Event::TkrPatCand*)=0;
+    virtual const Event::McParticle*      getBestMcParticle(const Event::TkrTrack*)=0;
 
     /// @brief Return TkrPatCand <-> McParticle "best" match
-    virtual const Event::TkrPatCand*      getBestTkrPatCand(const Event::McParticle*)=0;
+    virtual const Event::TkrTrack*        getBestTkrTrack(const Event::McParticle*)=0;
 
     /// @brief Return the number of hits on a given TkrPatCand track associated to
     ///        a given McParticle
-    virtual int                           getNumMcHits(const Event::TkrPatCand*, const Event::McParticle*)=0;
+    virtual int                           getNumMcHits(const Event::TkrTrack*, const Event::McParticle*)=0;
 };
 #endif
