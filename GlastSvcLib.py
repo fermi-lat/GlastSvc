006cc44f9dd1f431ba14b05a8eae88fb93706d5f
jrb <>
2012-08-17 00:04:14 +0000

    for Windows no-installed-headers

--- GlastSvcLib.py
+++ GlastSvcLib.py
@@ -1,7 +1,9 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/GlastSvc/GlastSvcLib.py,v 1.1 2008/07/09 21:13:42 glastrm Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/GlastSvc/GlastSvcLib.py,v 1.2 2009/08/07 00:54:57 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['GlastSvc'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'GlastSvc') 
     env.Tool('EventLib')
     env.Tool('xmlUtilLib')
     env.Tool('detModelLib')
